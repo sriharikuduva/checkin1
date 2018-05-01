235ba8352d4ae9fdf0f7ed5a572350f0d334269b
Maurice Chiu <32555827+MauriceChiu7@users.noreply.github.com>
2018-05-01 13:40:32 -0700

    Update BidderTest.java

--- JavaCode/BidderTest.java
+++ JavaCode/BidderTest.java
@@ -5,11 +5,13 @@ import java.util.*;
 public class BidderTest {
     Bidder bidder;
     Item item;
+    int amountAtLeastEqualToMinimum;
     
     @Before
     public void setUp() {
         bidder = new Bidder("John Smith", "john@uw.edu", "john@uw.edu", "123 Elm Street, Tacoma, WA", "2061234567", 10000);
         item = new Item("Sweater", 1, 50, "Blue Polka Dot Knit Sweater", "Path/To/File.jpg");
+        amountAtLeastEqualToMinimum = 55;
     }
     @Test
     public void placeBid_bidIsAtLeastEqualToMinimumBidForItem_True() {
