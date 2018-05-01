ee9de229d28d3a4ffc597048047554ffc850ed84
Maurice Chiu <32555827+MauriceChiu7@users.noreply.github.com>
2018-05-01 13:22:05 -0700

    Create Item.java

---
+++ JavaCode/Item.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Item {
+    private String name;
+    private int quantity;
+    private String  description;
+    private int startingPrice;
+    private String imagePath;
+    private ArrayList<Bid> bids;
+    
+    public Item(String name, int quantity, int startingPrice, String description, String imagePath) {
+        name = name;
+        quantity = quantity;
+        startingPrice = startingPrice;
+        description = description;
+        imagePath = imagePath;
+    }
+
+    public void addBid(Bid bid) {
+        bids.add(bid);
+    }
