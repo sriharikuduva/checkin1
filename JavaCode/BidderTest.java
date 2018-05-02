6c18e765a3aa1804df0e118b54e597321ca923fe
Shannon Weston <skw24@uw.edu>
2018-05-02 14:40:40 -0700

    adding .gitignore, BidderTest.java

--- JavaCode/BidderTest.java
+++ JavaCode/BidderTest.java
@@ -1,21 +1,43 @@
-import junit;
 import java.util.*;
-
+import org.junit.*;
+import Bidder.java;
 
 public class BidderTest {
-    Bidder bidder;
-    Item item;
-    int amountAtLeastEqualToMinimum;
-    
+    private Bidder bidder;
+    private Item item;
+    private int amountEqualToMinimum;
+    private int amountGreaterThanMinimum;
+    private int amountLessThanMinimum;     
+
+
+    //Group
     @Before
     public void setUp() {
         bidder = new Bidder("John Smith", "john@uw.edu", "john@uw.edu", "123 Elm Street, Tacoma, WA", "2061234567", 10000);
         item = new Item("Sweater", 1, 50, "Blue Polka Dot Knit Sweater", "Path/To/File.jpg");
-        amountAtLeastEqualToMinimum = 55;
+        amountEqualToMinimum = 50;
+        amountGreaterThanMinimum = 55;
+	amountLessThanMinimum = 49; 
     }
+
+    //Group
     @Test
-    public void placeBid_bidIsAtLeastEqualToMinimumBidForItem_True() {
-        bidder.placeBid(amountAtLeastEqualToMinimum, item);
+    public void placeBid_bidIsEqualToMinimumBidForItem_True() {
+        bidder.placeBid(amountEqualToMinimum, item);
         assertTrue(bidder.getBids().size() == 1);
+    }
+    
+    //Shannon Weston
+    @Test
+    public void placeBid_bidIsGreaterThanMinimumForItem_True() {
+        bidder.placeBid(amountGreaterThanMinimum, item);
+        assertTrue(bidder.getBids().size() == 1);
+    }
+    
+    //Shannon Weston
+    @Test
+    public void placeBid_bidIsLessThanMinimumForItem_False() {
+        bidder.placeBid(amountLessThanMinimum, item);
+        assertFalse(bidder.getBids().size() == 0);
     }
 }
