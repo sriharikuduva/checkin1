8c90f79b7b85bf4c1c3f35463111416690b8bd97
Baisalbek Urustanbekov <baisalbek4@gmail.com>
2018-05-07 23:08:31 -0700

    Unit Tets

---
+++ JavaCode/tests/DataControlCenterTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataControlCenterTest {
+
+    @Test
+    public void isBidderValid() {
+    }
+
+    @Test
+    public void isNonProfitValid() {
+    }
+
+    @Test
+    public void isRequestedAuctionDateValid() {
+    }
+
+    @Test
+    public void isRequestedAuctionDateAvailable() {
+    }
+
+    @Test
+    public void isTimeAvailable() {
+    }
+}
